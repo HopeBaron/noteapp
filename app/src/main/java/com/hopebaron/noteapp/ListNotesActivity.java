5a794f4a7011f0e0161be4bf912985bc8c60a3f6
HopeBaron <truehope9000@gmail.com>
2022-02-25 14:50:54 +0200

    search implementation

--- app/src/main/java/com/hopebaron/noteapp/ListNotesActivity.java
+++ app/src/main/java/com/hopebaron/noteapp/ListNotesActivity.java
@@ -2,6 +2,7 @@ package com.hopebaron.noteapp;
 
 import android.content.Intent;
 import android.view.View;
+import android.widget.SearchView;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,32 +12,53 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class ListNotesActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     NoteRepository repository;
     TextView messageView;
+    SearchView searchView;
     List<Note> notes = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_notes);
+        searchView = findViewById(R.id.notes_search_view);
         recyclerView = findViewById(R.id.recycler_view);
         messageView = findViewById(R.id.no_notes_message);
         repository = NoteRepository.getInstance(getApplicationContext());
         setupRecycleView(notes);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String terms) {
+                NoteListAdapter adapter = (NoteListAdapter) recyclerView.getAdapter();
+                adapter.getFilter().filter(terms);
+                return false;
+            }
+        });
     }
 
     @Override
     protected void onResume() {
+
         super.onResume();
+        searchView.setVisibility(View.VISIBLE);
         recyclerView.setVisibility(View.VISIBLE);
         messageView.setVisibility(View.INVISIBLE);
         notes.clear();
         notes.addAll(repository.getAll());
-        recyclerView.getAdapter().notifyDataSetChanged();
+        NoteListAdapter adapter = (NoteListAdapter) recyclerView.getAdapter();
+        adapter.getFilter().filter(searchView.getQuery());
         if(notes.isEmpty()) {
+            searchView.setVisibility(View.INVISIBLE);
             recyclerView.setVisibility(View.INVISIBLE);
             messageView.setVisibility(View.VISIBLE);
         }
@@ -61,5 +83,6 @@ public class ListNotesActivity extends AppCompatActivity {
         intent.putExtra("noteId", String.valueOf(note.getId()));
         startActivity(intent);
     }
+
 
 }
--- app/src/main/java/com/hopebaron/noteapp/NoteListAdapter.java
+++ app/src/main/java/com/hopebaron/noteapp/NoteListAdapter.java
@@ -1,34 +1,43 @@
 package com.hopebaron.noteapp;
 
-import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
+import android.widget.Filter;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
-import org.w3c.dom.Text;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 public class NoteListAdapter  extends RecyclerView.Adapter<NoteListAdapter.ViewHolder> {
-    private final List<Note> notes;
+    private final List<Note> viewNotes = new ArrayList<>();
+    private List<Note> notes;
+    private final Filter filter = new NotesFilter();
     private ItemPressedListener listener = null;
-    private static final int RGB_MAX_RAND = 256;
+
     public NoteListAdapter(List<Note> notes) {
         this.notes = notes;
+        this.viewNotes.addAll(this.notes);
     }
+
+    public Filter getFilter() {
+        return filter;
+    }
+
     public void setOnItemPressedListener(ItemPressedListener listener) {
         this.listener = listener;
     }
     public class ViewHolder extends RecyclerView.ViewHolder {
+
         private final CardView cardView;
         private final TextView titleText;
         private final TextView contentText;
+
         private final TextView dateText;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             cardView = itemView.findViewById(R.id.card_view);
@@ -53,12 +62,13 @@ public class NoteListAdapter  extends RecyclerView.Adapter<NoteListAdapter.ViewH
             return cardView;
         }
 
-
         public void bind(int position, Note note) {
-            if(listener == null) return;
-            cardView.setOnClickListener(view -> listener.listener(position,note));
+            if (listener == null) return;
+            cardView.setOnClickListener(view -> listener.listener(position, note));
         }
+
     }
+
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -67,7 +77,7 @@ public class NoteListAdapter  extends RecyclerView.Adapter<NoteListAdapter.ViewH
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        Note note = notes.get(position);
+        Note note = viewNotes.get(position);
         holder.getTitleText().setText(note.getTitle());
         holder.getContentText().setText(note.getContent());
         holder.getDateText().setText(note.getDate());
@@ -76,6 +86,36 @@ public class NoteListAdapter  extends RecyclerView.Adapter<NoteListAdapter.ViewH
 
     @Override
     public int getItemCount() {
-        return notes.size();
+        return viewNotes.size();
+    }
+    private class NotesFilter extends Filter {
+
+
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            List<Note> filteredList = new ArrayList<>();
+            if (constraint == null || constraint.length() == 0) {
+                filteredList.addAll(notes);
+            } else {
+                String filterPattern = constraint.toString().toLowerCase().trim();
+                for (Note note : notes) {
+                    boolean titleMatchPattern = note.getTitle().toLowerCase().contains(filterPattern);
+                    boolean contentMatchPattern = note.getContent().toLowerCase().contains(filterPattern);
+                    if (titleMatchPattern || contentMatchPattern) {
+                        filteredList.add(note);
+                    }
+                }
+            }
+            FilterResults results = new FilterResults();
+            results.values = filteredList;
+            return results;
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            viewNotes.clear();
+            viewNotes.addAll((List) results.values);
+            notifyDataSetChanged();
+        }
     }
 }
